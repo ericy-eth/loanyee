0af4ea24de6173405e42bdd238067b78a813b822
BlueGlaucus <pomp.bathos0r@icloud.com>
2022-09-23 00:47:17 -0700

    fixed syntax error in source mapping file

--- subgraph/loanyee/src/loan-factory.ts
+++ subgraph/loanyee/src/loan-factory.ts
@@ -17,8 +17,8 @@ export function handleloanCreated(event: loanCreated): void {
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
+  entity.count = entity.count.plus(BigInt.fromI32(1))
+  
   // Entity fields can be set based on event parameters
   entity.borrowAmount = event.params.borrowAmount
   entity.interestRate = event.params.interestRate
